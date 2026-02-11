8e3ffc04ffa68d0c9d179e786abec0c3972bd8eb
Shaikh Shirin <skshirin007@gmail.com>
2026-02-11 18:00:29 +0530

    add forgot password utility

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,6 +2,9 @@ import { errorhandler, errorMiddleware } from "../middlewares/errorMiddleware.js
 import { catchAsyncError } from "../middlewares/catchAsyncError.js";
 import database from "../database/db.js";
 import { sendToken } from "../utils/jwtToken.js";
+import {generateResetPasswordToken} from "../utils/generateResetPasswordToken.js";
+import { generateForgotPasswordEmailTemplate } from "../utils/generateForgotPasswordEmailTemplate.js";
+import {sendEmail} from "../utils/sendEmail.js";
 import bcrypt from "bcryptjs";
 
 export const register = catchAsyncError(async (req,res,next) =>{
@@ -60,4 +63,50 @@ export const logout = catchAsyncError(async (req,res,next) =>{
         success: true,
         message: "Logged out successfully"
     });
+});
+
+export const forgotPassword = catchAsyncError(async (req,res,next) =>{
+    const { email } = req.body;
+    const { frontendUrl } = req.body;
+    let userResult = await database.query
+        (`SELECT * FROM users WHERE email = $1`,
+        [email]
+    );
+    if(userResult.rows.length === 0){
+        return next(new errorhandler("User not found with this email",404));
+    }
+    const user = userResult.rows[0];
+    const { resetToken, hashedToken, resetPasswordExpireTime } =
+        generateResetPasswordToken();
+
+    await database.query(
+        `UPDATE users SET reset_password_token = $1,
+         reset_password_expire = to_timestamp($2) WHERE email = $3`,
+        [hashedToken, resetPasswordExpireTime / 1000, email]
+    );
+    
+    const resetPasswordUrl = `${frontendUrl}/password/reset/${resetToken}`;
+
+    const message = generateForgotPasswordEmailTemplate(resetPasswordUrl);
+
+    try{
+        await sendEmail({
+            email: user.email,
+            subject: "Ecommerce Password Recovery",
+            message,
+        });
+        res.status(200).json({
+            success: true,
+            message: `Email sent to ${user.email} successfully`,
+        });
+    }
+    catch(error){
+        console.log("EMAIL ERROR:", error);
+        await database.query(
+            `UPDATE users SET reset_password_token = NULL,
+             reset_password_expire = NULL WHERE email = $1`,
+            [email]
+        );
+        return next(new errorhandler("Email could not be sent",500));
+    }
 });
--- router/auth_Routes.js
+++ router/auth_Routes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { login, register, logout, getUser } from '../controllers/authController.js';
+import { login, register, logout, getUser, forgotPassword } from '../controllers/authController.js';
 import { isAuthenticated } from '../middlewares/authMiddleware.js';
 
 const router = express.Router();
@@ -12,5 +12,6 @@ router.get('/logout', isAuthenticated, logout);
 
 router.get('/me', isAuthenticated, getUser);
 
+router.post('/password/forgot', forgotPassword);
 
 export default router;
