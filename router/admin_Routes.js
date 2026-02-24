aa0673ac9c80eee38ef462be50fc4c8df1cc1fe4
Shaikh Shirin <skshirin007@gmail.com>
2026-02-24 23:26:35 +0530

    admin controller

--- router/admin_Routes.js
+++ router/admin_Routes.js
@@ -1,9 +1,13 @@
 import express from 'express';
-import { getAllUsers, deleteUser } from '../controllers/admin_Controller.js';
-import { isAuthenticated, authorizeRoles } from '../middlewares/authMiddleware.js';
+import { getAllUsers, deleteUser, dashboardStats } from '../controllers/adminController.js';
+import { isAuthenticated, authorizedRoles } from '../middlewares/authMiddleware.js';
 
 const router = express.Router();
 
-router.get('/users', isAuthenticated, authorizeRoles('admin'), getAllUsers);
+router.get('/getAllUsers', isAuthenticated, authorizedRoles('admin'), getAllUsers);
+
+router.delete('/deleteUser/:id', isAuthenticated, authorizedRoles('admin'), deleteUser);
+
+router.get('/fetch/dashboard-stats', isAuthenticated, authorizedRoles('admin'), dashboardStats);
 
 export default router;
