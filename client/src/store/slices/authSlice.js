8a406ad452ea964f408c32afc74b90bb9200b9fb
Shaikh Shirin <skshirin007@gmail.com>
2026-04-16 21:03:43 +0530

    auth slice

--- client/src/store/slices/authSlice.js
+++ client/src/store/slices/authSlice.js
@@ -2,17 +2,37 @@ import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import { axiosInstance } from "../../lib/axios";
 import { toast } from "react-toastify";
 
-const authSlice = createSlice({
-  name: "auth",
-  initialState: {
-    authUser: null,
-    isSigningUp: false,
-    isLoggingIn: false,
-    isUpdatingProfile: false,
-    isUpdatingPassword: false,
-    isRequestingForToken: false,
-    isCheckingAuth: true,
-  },
+ export const register = createAsyncThunk(
+  "auth/register",
+  async (data, { thunkApi }) => {
+  });
+
+ export const login = createAsyncThunk(
+  "auth/login",
+  async (data, { thunkApi }) => {
+  });
+
+  export const getUser = createAsyncThunk(
+  "auth/getUser",
+  async (_, { thunkApi }) => {
+  });
+
+  export const logout = createAsyncThunk(
+  "auth/logout",
+  async (_, { thunkApi }) => {
+  });
+
+ const authSlice = createSlice({
+   name: "auth",
+   initialState: {
+      authUser: null,
+      isSigningUp: false,
+      isLoggingIn: false,
+      isUpdatingProfile: false,
+      isUpdatingPassword: false,
+      isRequestingForToken: false,
+      isCheckingAuth: true,
+   },
   extraReducers: (builder) => {},
 });
 
